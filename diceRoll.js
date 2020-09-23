551d20088262778b92e8886dc8f0bb093c2d74a7
Diana.Cazales <dianacazales>
2020-09-23 12:36:27 -0500

    Update on adding files

---
+++ diceRoll.js
@@ -0,0 +1,34 @@
+
+
+
+
+function RollSixSidedDice() {
+    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
+    document.getElementById("dice1").value = Math.floor(Math.random()*6) + 1;
+    document.getElementById("dice2").value = Math.floor(Math.random()*6) + 1;
+    document.getElementById("dice3").value = Math.floor(Math.random()*6) + 1;
+    document.getElementById("dice4").value = Math.floor(Math.random()*6) + 1;
+
+}
+
+
+function RollDice() {
+    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
+    
+}
+function RollDice1() {
+    document.getElementById("dice1").value = Math.floor(Math.random()*6) + 1;
+    
+}
+function RollDice2() {
+    document.getElementById("dice2").value = Math.floor(Math.random()*6) + 1;
+    
+}
+function RollDice3() {
+    document.getElementById("dice3").value = Math.floor(Math.random()*6) + 1;
+    
+}
+function RollDice4() {
+    document.getElementById("dice4").value = Math.floor(Math.random()*6) + 1;
+    
+}
